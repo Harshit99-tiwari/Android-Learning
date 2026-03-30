3f89617b5adda5ecd52a4f5a9e05442b0419e369
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-03-30 17:08:32 +0530

    Initial commit - Android learning project

---
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.demoapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+    }
