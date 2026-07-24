4edaad253815408a9680656e5638c5db91018e3c
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-07-24 13:23:32 +0530

    Explicit intent:opening webPage and Camera

--- app/src/main/java/com/example/demoapp/Secondactivity.kt
+++ app/src/main/java/com/example/demoapp/Secondactivity.kt
@@ -1,8 +1,12 @@
 package com.example.demoapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,6 +15,19 @@ class Secondactivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_secondactivity)
+
+        val WebButton = findViewById<CardView>(R.id.cardweb)
+        val CamButton =  findViewById<CardView>(R.id.cardCamera)
+
+        WebButton.setOnClickListener {
+            val intent =  Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://github.com/Harshit99-tiwari")
+            startActivity(intent)
+        }
+        CamButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
